fe4b53c6cfdcd5f4c535b6dab4e52c39d7c41daa
Vamshidhar Dawoor <vamshidhar.dawoor@perficient.com>
2026-06-16 17:45:10 +0530

    build issue fixed

--- src/components/Car.tsx
+++ src/components/Car.tsx
@@ -1,7 +1,7 @@
 import { useGLTF } from '@react-three/drei';
-import type { GroupProps } from '@react-three/fiber';
+import type { JSX } from 'react';
 
-export function Car(props: GroupProps) {
+export function Car(props: JSX.IntrinsicElements['group']) {
   const { scene } = useGLTF('/car1.glb');
   return <primitive object={scene} scale={1.5} {...props} />;
 }
