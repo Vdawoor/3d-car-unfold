69f98ad1790d8aeaedab3be5d228171309cf1492
Vamshidhar Dawoor <vamshidhar.dawoor@perficient.com>
2026-06-16 18:01:08 +0530

    deployment configuration

--- src/components/Car.tsx
+++ src/components/Car.tsx
@@ -2,8 +2,8 @@ import { useGLTF } from '@react-three/drei';
 import type { JSX } from 'react';
 
 export function Car(props: JSX.IntrinsicElements['group']) {
-  const { scene } = useGLTF('/car-unfold-3d/car1.glb');
+  const { scene } = useGLTF('/3d-car-unfold/car1.glb');
   return <primitive object={scene} scale={1.5} {...props} />;
 }
 
-useGLTF.preload('/car-unfold-3d/car1.glb');
+useGLTF.preload('/3d-car-unfold/car1.glb');
